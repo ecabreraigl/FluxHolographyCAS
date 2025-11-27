b85ca63ab7958ccaa221c334483295eed21c9134
ecabreraigl <ecabrera.igl@gmail.com>
2025-11-27 12:28:06 -0300

    Create __init__.py for Flux Holography CAS package
    Initialized the cas package with module listings.

---
+++ cas/__init__.py
@@ -0,0 +1,23 @@
+"""
+Flux Holography CAS Package
+---------------------------
+Contains all computational algebra systems (CAS) for the FH framework.
+Modules:
+ - fh_core_cas.py
+ - fh_horizons_cosmo_cas.py
+ - fh_tick_noneq_cas.py
+ - fh_selection_integrability_iw_cas.py
+ - fh_corollaries_cas.py
+ - fh_complementary_cas.py
+ - fh_master_cas.py
+"""
+
+__all__ = [
+    "fh_core_cas",
+    "fh_horizons_cosmo_cas",
+    "fh_tick_noneq_cas",
+    "fh_selection_integrability_iw_cas",
+    "fh_corollaries_cas",
+    "fh_complementary_cas",
+    "fh_master_cas"
+]
